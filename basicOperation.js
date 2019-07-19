4674408240be3e69f0ab11efec99e0cbafb30974
amita.jethani <amitajethani@gmail.com>
2019-07-20 00:50:15 +0530

    Incremental and Remainder Operator

---
+++ basicOperation.js
@@ -0,0 +1,16 @@
+//Incremental operator
+var myVar = 10;
+console.log(myVar);
+myVar = 10+1;
+console.log(myVar);
+myVar++;
+console.log(myVar);
+//to get even or odd number
+var remain = 12;
+console.log(remain);
+var remain2 = 13;
+console.log(remain2);
+remain = 12/2;//0 the number is even
+console.log(remain);
+remain = 13/2;//1 the number is odd
+console.log(remain2);
